7b56b89029a371a269757cff3496bd1e5d1b793d
jmartinezarce <jmartinezarce@c7f73ec5-423e-0410-970b-39fc7cea833a>
2007-11-17 19:52:36 +0000

    Traduccion de fechas fue extraida y movida al plugin insignia_tools.

--- init.rb
+++ init.rb
@@ -1,6 +1,3 @@
-Date::MONTHNAMES = ["", "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-Date::DAYNAMES = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"]
-
 require 'validations_translation'
 require 'administrate_me'
 require 'test_administrate_me'
