0806d3fb6a057e928e81436aef89b9d40b8aa2a2
ckozus <ckozus@c7f73ec5-423e-0410-970b-39fc7cea833a>
2007-11-25 07:11:23 +0000

    Corrección para Issue #18: admin_view ahora es dinámico. Utiliza los campos definidos en el modelo.

--- generators/admin_view/admin_view_generator.rb
+++ generators/admin_view/admin_view_generator.rb
@@ -1,12 +1,19 @@
 class AdminViewGenerator < Rails::Generator::NamedBase
+  attr_accessor :klass, :form_type
   
   def initialize(*args)
     super
+    @klass = name.classify.constantize
+    @form_type = Hash.new('text_field')
+    @form_type[:text] = 'text_area'
+    @form_type[:boolean] = 'check_box'
+    @form_type[:datetime] = 'datetime_select'
+    @form_type[:date] = 'date_select'
   end
 
   def manifest
     record do |m|
-      views_dir = File.join('app/views', name.downcase.pluralize )
+      views_dir = File.join('app/views', name.underscore.pluralize )
       m.directory views_dir
       m.template '_form.html.erb', File.join(views_dir, "_form.html.erb")
       m.template '_list.html.erb', File.join(views_dir, "_list.html.erb")
