926ba081637837aa8f12464a26cc9670d860bbad
jmartinezarce <jmartinezarce@c7f73ec5-423e-0410-970b-39fc7cea833a>
2007-12-01 05:39:03 +0000

    corrección

--- lib/admin_scaffold.rb
+++ lib/admin_scaffold.rb
@@ -88,8 +88,8 @@ module AdministrateMe::AdminScaffold
     
     def new    
       if self.class.accepted_action(:new)
-        mclass = ( options[:model] ? options[:model] : controller_name).classify.constantize
-        instance_variable_set("@resource", mclass.new)                call_before_render
+        mclass = ( (options[:model]) ? options[:model] : controller_name ).classify.constantize
+        instance_variable_set("@resource", mclass.new)        call_before_render
         render :template => 'commons/new'
       else
         not_available
