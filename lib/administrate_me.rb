1f19ffe21ac7f84171b9051ad25286d7ecd9b8e6
jmartinezarce <jmartinezarce@c7f73ec5-423e-0410-970b-39fc7cea833a>
2007-12-05 03:09:03 +0000

    agrego un poco de documentación para los métodos relevantes del plugin. (en ingles)

--- lib/administrate_me.rb
+++ lib/administrate_me.rb
@@ -1,19 +1,19 @@
 module AdministrateMe
   
   module ClassMethods
-  
-    # Crea en el controller en el que se lo utiliza los métodos necesarios para
-    # que se responda a las acciones restful del recurso con el que trabaja.
-    # 
-    # ==== Ejemplo
+    # Use this macro to include all the necesary methods that a controller
+    # needs to support a Restfull administration of an specific resource.
+    #
+    # ==== Example
     #   
-    #   class LugaresController < ApplicationController
+    #   class ProductsController < ApplicationController
     #     administrate_me do
-    #       search :nombre, :descripcion, :direccion, :localidad
-    #       order  'nombre'
+    #       search :name, :description, :price
+    #       order  'name'
     #     end
     #   end
-    #
+    # 
+    # You'll need to add map.resources :products to the routes.rb file.
     def administrate_me(options = {})
       @administrate_me_options = {}
       @administrate_me_options[:secured] = true
@@ -35,10 +35,37 @@ module AdministrateMe
       }
     end
     
+    # Sometimes it's necesary to include controllers in the application
+    # which don't need to include all Restful actions. In this particular
+    # case, you can specify this in the plugin macro with the options
+    # 'no_scaffold!'.
+    # 
+    #   class DashboardController < ApplicationController
+    #     administrate_me do
+    #       no_scaffold!
+    #     end
+    #   end
+    # 
+    # In this case, the controller Dashboard loads the basic features of 
+    # the plugin, but does not include the Restful methods.
     def no_scaffold!
       @administrate_me_options[:scaffold] = false
     end
     
+    # The except method specifies the action that will not be allowed
+    # in the controller.
+    #
+    # ==== Example
+    #   
+    #   class ProductsController < ApplicationController
+    #     administrate_me do
+    #       search :name, :description, :price
+    #       except :new, :edit, :destroy
+    #     end
+    #   end
+    # 
+    # With this configuration, the plugin load a read-only controller, that will only
+    # accept the index and the show actions.
     def except(*actions)
       @administrate_me_options[:except] = actions
     end
@@ -51,10 +78,13 @@ module AdministrateMe
       @administrate_me_options[:order_by] = criteria
     end
     
+    # Use per_page to indicates the number of records to be listed per page.
     def per_page(records)
       @administrate_me_options[:per_page] = records
     end
     
+    # The public_access! method specifies that the controller will not require user
+    # authetication.
     def public_access!
       @administrate_me_options[:secured] = false
     end
