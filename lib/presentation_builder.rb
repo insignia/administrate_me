39f6f553f52935d278c2c1b67a3792a4bdf7f00e
Carlos Kozuszko <carlos@insignia4u.com>
2008-07-26 16:11:21 -0300

    Finished porting list_builder_for wiki documentation to rdoc.

--- lib/presentation_builder.rb
+++ lib/presentation_builder.rb
@@ -6,16 +6,16 @@ module AdminView::PresentationBuilder
   # If you happen to use columns in your models named the same as its few public
   # methods (like gpb_columns and gpb_data) err... there would be some problems.
   class GridPresentationBuilder
-    class ListColumn      
+    class ListColumn
       def initialize(field, options={})
         @field = field
         @options = options
       end
-      
+
       def value_for(item)
         @options[:with] ? related_value_for(item.send(@field)) : item.send(@field)
       end
-      
+
       def related_value_for(value)
         if value.is_a?(Array)
           rtn = value.map{|x| x.send(@options[:with])}.join(", ")
@@ -24,16 +24,16 @@ module AdminView::PresentationBuilder
         end
         rtn
       end
-      
+
       def caption
         @options[:caption] || @field.to_s.titleize
       end
-      
+
       def style
         @options[:style] = ""
         @options[:style] << custom_grid unless custom_grid.empty?
       end
-      
+
       def custom_grid
         customizations = []
         customizations << "width:#{@options[:width]}"      if @options[:width]
@@ -43,84 +43,112 @@ module AdminView::PresentationBuilder
         customizations.join(';')
       end
     end
-    
+
     def initialize(collection)
       @collection = collection
       @columns = []
       @rows    = []
       @captions = {}
-    end        
+    end
 
     # Returns the columns included on the grid.
     def gpb_columns
       @columns
     end
-    
+
     # Returns the dataset used on the grid.
     def gpb_data
       @collection
     end
-   
+
     def method_missing(field, options={})
-      @columns << ListColumn.new(field, options)      
+      @columns << ListColumn.new(field, options)
     end
-    
-  end    
-  
-  # list_builder_for is a helper to easily generate grids on index actions, 
+
+  end
+
+  # list_builder_for is a helper to easily generate grids on index actions,
   # even though it can be used elsewhere.
-  # 
+  #
   # == Example
-  # 
+  #
   #   <%= list_builder_for @records do |list|
   #         list.name
   #         list.last_name
   #       end -%>
-  #       
+  #
   # At the administrate_me index action have the @records instance which holds
   # all the records that have to be displayed. On each instance we will call
   # the name and last_name methods over the <code>list</code> variable, this
   # will yield the inclusion of those two fields as columns on the generated
   # grid.
-  # 
-  # As an implementation detail, the list variable passed to a block, is an
-  # instance of <code>GridPresentationBuilder</code> that is instantiated before 
+  #
+  # As an implementation detail, the +list+ variable passed to a block, is an
+  # instance of <code>GridPresentationBuilder</code> that is instantiated before
   # iterating over the records and holds the configuration for the grid.
-  # 
+  #
   # == Advanced options
-  # 
+  #
   # ==== Accessing data through associations
-  # 
+  #
   # You can also include columns with data that is not accessible with just one
   # method call on each records. Let's say you have a list of @records where each
-  # item represents a product, those products are related to a brand using a 
+  # item represents a product, those products are related to a brand using a
   # belongs_to association and you just want to include a column with the brand
-  # name. 
-  # On cases like this you'll need to include :with option on the call to the
-  # list method. See an example:
-  # 
+  # name.
+  # On cases like this you'll need to include +:with+ option on the method call 
+  # on the +list+ method. See an example:
+  #
   #   <%= list_builder_for @records do |list|
   #         list.name
   #         list.brand :with => :name
   #       end -%>
-  #       
+  #
   # ==== Assigning column titles
-  # 
-  # You'll have to use the :caption option:
-  # 
+  #
+  # You'll have to use the +:caption+ option:
+  #
   #   <%= list_builder_for @records do |list|
   #         list.name :caption => 'Product name'
   #         list.brand :with => :name, :caption => 'Brand name'
   #       end -%>
-  #       
+  #
   # By default, list_builder_for will use the titleized field names.
+  #
+  # ==== Assigning format to column
+  #
+  # The calls made to the +list+ variable to define columns on the grid also
+  # accept options to change the format of the data displayed. This options set 
+  # the style attribute of the <td> element of each element on that column.
+  #
+  # Valid options:
+  # * <tt>:width</tt> - Width of the column
+  # * <tt>:color</tt> -  Text color of the column.
+  # * <tt>:string</tt> -  Shows the results strongs (bold) if the value is true.
+  # * <tt>:align</tt> -  Sets the text-align attribute for the column.
+  #
+  #    <%= list_builder_for @records do |list|
+  #         list.name        :width => '250px', :color => '#666666', :strong => true     
+  #         list.description
+  #         list.price       :width => '100px', :align => :right
+  #      end %>
+  #
+  # ==== Report mode
+  # 
+  # Using this mode the grid will be displayed without showing links to the actions
+  # on each row.
+  #   
+  #   <%= list_builder_for @libros_mas_vendidos, :report => true do |list|
+  #         list.nombre
+  #         list.total_ventas
+  #       end %>  
   #
   def list_builder_for(collection, options = {}, type = :grid)
     yield(list = GridPresentationBuilder.new(collection))
-    list_renderer(list, options, type)    
+    list_renderer(list, options, type)
   end
-  
-  # Internally used by list_builder_for. Creates html output including a 
+
+  # Internally used by list_builder_for. Creates html output including a
   # flash message, the grid and the pagination when will_paginate is available.
   def list_renderer(list, options, type)
     html  = ""
@@ -145,30 +173,30 @@ module AdminView::PresentationBuilder
     pb.gpb_columns.each{|column| html << "<th> #{column.caption} </th>" }
     "<tr> #{ html } </tr>"
   end
-  
+
   def build_grid_body(pb, options)
     html = ""
     pb.gpb_data.each{|item| html << build_row_for(pb, item, cycle('odd', 'even'), options) }
     html
   end
-  
+
   def build_row_for(pb, item, css_class, options)
     html = ""
-    pb.gpb_columns.each do |column| 
-      html << "<td style='#{column.style}'> #{column.value_for(item)} </td>"      
-    end             
+    pb.gpb_columns.each do |column|
+      html << "<td style='#{column.style}'> #{column.value_for(item)} </td>"
+    end
     html << "<td class='link_options'> #{build_row_links(item)} </td>" unless options[:report]
     "<tr class='#{css_class}'> #{html} </tr>"
   end
-  
+
   def build_row_links(item)
-    html = ""    
+    html = ""
     html << link_to(image_tag('admin_ui/show.png'), path_to_element(item), :title => 'ver más...') if controller.class.accepted_action?(:show)
     html << link_to(image_tag('admin_ui/edit.png'), path_to_element(item, :prefix => :edit), :title => 'editar este registro') if controller.class.accepted_action?(:edit)
     html << link_to(image_tag('admin_ui/destroy.png'), path_to_element(item), :confirm => 'El registro será eliminado definitivamente. ¿Desea continuar?', :method => :delete, :title => 'eliminar este registro') if controller.class.accepted_action?(:destroy)
-    html 
+    html
   end
-  
+
 end
 
 ActionView::Base.send :include, AdminView::PresentationBuilder
