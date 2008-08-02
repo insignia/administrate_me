52c8fd67b1dfdd9ae386da518619fb2c1296edb3
Carlos Kozuszko <carlos@insignia4u.com>
2008-08-02 16:11:56 -0300

    Adding administrate_me_spec.rb

---
+++ spec/administrate_me_spec.rb
@@ -0,0 +1,16 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+require File.dirname(__FILE__) + '/../lib/administrate_me.rb'
+
+describe AdministrateMe do
+  before(:each) do
+    @administrate_me = AdministrateMe.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
