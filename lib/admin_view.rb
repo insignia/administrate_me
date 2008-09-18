f9c2bf0900528938f03ea52f0a77f86e08b6b553
Juan María Martínez Arce <jmax@jmax-laptop.(none)>
2008-09-18 13:01:42 -0300

    [admin_view] add 'resource_card' helper and modify admin stylesheet

--- lib/admin_view.rb
+++ lib/admin_view.rb
@@ -1,5 +1,51 @@
 module AdminView
 
+  # resource_card helper
+  # this helper render a very elegant presentation card for a resource.
+  #   
+  # <% resource_card do %>  
+  #   <%= row_for  "Name",        @resource.name %>
+  #   <%= row_for  "Name",        @resource.name %>  
+  #   <%= area_for "Description", @resource.description %>
+  #   <%= row_for  "Name",        @resource.name %>
+  #   <%= row_for  "Date",        Date.today.to_s %>
+  #   <%= render_buttons %>  
+  # <% end %>
+  #
+  def resource_card(&block)
+    content = capture(&block)
+    concat(show_section_label,                block.binding)
+    concat('<div class="resource-card">',     block.binding)
+    concat(content,                           block.binding)
+    concat('</div>',                          block.binding)
+    concat('<div style="clear:both;"></div>', block.binding)
+  end
+  
+  def row_for(label, value)
+    html  = '<div class="rc-block">'
+    html << '  <div class="rc-row rc-caption">'+label+'</div>'
+    html << '  <div class="rc-row rc-content">'+value+'</div>'
+    html << '</div>'
+    html
+  end
+  
+  def area_for(label, value)
+    html  = '<div class="rc-block">'
+    html << '  <div class="rc-area rc-label">'+label+'</div>'
+    html << '  <div class="rc-area rc-detail">'+value+'</div>'
+    html << '</div>'
+    html
+  end
+  
+  def render_buttons
+    html  = '<div class="buttons">'
+    html << edit_action    if controller.class.accepted_action?(:edit)
+    html << destroy_action if controller.class.accepted_action?(:destroy)
+    html << back_action
+    html << '</div>'
+    html
+  end  
+
   # field_block helper
   # 
   # Use this helper to set a two column fieldset layout for an admin form.
