1b0fc7130aa2066ce456b27e459633f397751c1e
Carlos Kozuszko <carlos@insignia4u.com>
2008-11-26 22:40:42 -0200

    Updating initializer template to support new I18n syntax

--- files/initializers/administrate_me.rb
+++ files/initializers/administrate_me.rb
@@ -1,6 +1,5 @@
-I18n.populate do
-  # Change the languange of your app specifying another file on the locale folder.
-  # Also change the folder name if the administrate_me plugin was installed on a
-  # different folder name.
-  require 'vendor/plugins/administrate_me/locale/en-US.rb'
-end
+I18n.load_path << 'vendor/plugins/administrate_me/locale/sp-AR.rb'
+I18n.load_path << 'vendor/plugins/administrate_me/locale/en-US.rb'
+
+# Don't forget to set your default locale on environment.rb with this line:
+# config.i18n.default_locale = :sp
