71c2839304aca834e2c1cfce4ad1d3cb0808d70e
Carlos Kozuszko <carlos@insignia4u.com>
2008-11-27 13:45:27 -0200

    - Adding portuguese translations. Thanks to Flavio Ruiz Alemandi. - Fixing encoding problem on sp-AR.rb

---
+++ locale/pt-BR.rb
@@ -0,0 +1,39 @@
+{:pt => {
+  :yes => 'SIM',
+  :no  => 'NÃO',
+  :views => {
+    :prev => 'Anterior',
+    :next => 'Seguinte',
+    :logout => 'Sair',
+    :save_this_changes => 'Salvar alterações',
+    :cancel => 'Cancelar',
+    :add_new_record => 'Agregar novo registro',
+    :default_title => 'Administração',
+    :download_to_excel => "Descarregar no Excel",
+    :see_more => 'ver mais...', 
+    :edit_this_record => 'Editar este registro',
+    :delete_confirm => 'O registro será excluido definitivamente. Deseja continuar?',
+    :delete_this_record => 'Excluir este registro',
+    :filter_records_by => 'Filtrar registros por...',
+    :admin => 'administrar',
+    :empty_dataset => 'Não ha registros carregados',
+    :new_record => "Novo {{model}}",
+    :edit_record => "Editando um {{model}}",
+    :back => 'Voltar',
+    :search_prompt => 'Pesquisar (digite a palavra que procura)',
+    :support_tasks => "Tarefas de Suporte",
+    :filter_by => "Filtrar registros por...",
+    :filter_show_all => 'Todos'
+  },
+  :messages => {
+    :create_success => 'O registro foi criado com sucesso.',
+    :save_success => 'As alterações foram salvas com sucesso.',
+    :destroy_success => 'O registro foi excluido com sucesso.',
+    :search_message => "foram encontrados {{count}} resultados com \"<b>{{search_key}}</b>\"",
+    :missing_parent => "Não existe o elemento padrão solicitado."
+  },
+  :errors => {
+    :modules_not_defined => "Defina os módulos do aplicativo. Consulte a documentação de AdministrateMe::Base#set_module para mais informações",
+    :exception_on_save => "Ocorreu um erro ao salvar o registro: {{message}}"
+  }
+}}
--- locale/sp-AR.rb
+++ locale/sp-AR.rb
@@ -5,10 +5,10 @@
     :prev => 'Anterior',
     :next => 'Siguiente',
     :logout => 'Salir',
-    :save_this_changes => 'Guardar estos cambios!!!!!',
+    :save_this_changes => 'Guardar estos cambios',
     :cancel => 'Cancelar',
     :add_new_record => 'Agregar nuevo registro',
-    :default_title => 'Administraci�n',
+    :default_title => 'Administración',
     :download_to_excel => "Descargar a Excel",
     :see_more => 'ver más...',
     :edit_this_record => 'Editar este registro',
@@ -33,7 +33,7 @@
     :missing_parent => "No existe el padre del elemento solicitado"
   },
   :errors => {
-    :modules_not_defined => "Debe definir los m�dulos para la aplicaci�n. Ver documentacion de AdministrateMe::Base#set_module para mayor informaci�n",
-    :exception_on_save => "Ocurri� una exception al salvar el registro: {{message}}"
+    :modules_not_defined => "Debe definir los módulos para la aplicación. Ver documentacion de AdministrateMe::Base#set_module para mayor información",
+    :exception_on_save => "Ocurrió una exception al salvar el registro: {{message}}"
   }
 }}
