e7028c2f28c60e9f99b2dd7c3c2032fbff15f83d
Carlos Kozuszko <carlos@insignia4u.com>
2008-12-03 18:36:49 -0200

    Improvements on shared behaviour spec:   - @will_paginate_installed instance variable is no longer required.   - A 'basic administrate_me with parent' behaviour was added to test child controllers.

--- lib/shared_behaviours.rb
+++ lib/shared_behaviours.rb
@@ -2,13 +2,24 @@
 # controllers.
 # Here's an example:
 #
+#   # for a simple administrate_me controller
 #   describe ProjectsController do
 #     before do
 #       @mock_model_class = Project
-#       @will_paginate_installed = @mock_model_class.respond_to?('paginate')
 #     end
 #
 #     it_should_behave_like 'basic administrate_me'
+#
+#   end
+#
+#   # for a child administrate_me controller
+#   describe ProjectsController do
+#     before do
+#       @mock_model_class = Project
+#       @parent_name = 'company'
+#     end
+#
+#     it_should_behave_like 'basic administrate_me with parent'
 #
 #   end
 #
@@ -30,17 +41,21 @@
 describe 'basic administrate_me', :shared => true do
 
   def mock_model_instance(stubs={})
-    @mock_model_instance ||= mock_model(@mock_model_class, stubs)
+    @mock_model_instance ||= mock_model(@mock_model_class, stubs)    
+  end
+
+  def will_paginate_installed?
+    @mock_model_class.respond_to?('paginate')
   end
 
   describe "responding to GET index" do
 
     it "should expose all models as @records" do
-      if @will_paginate_installed
+      if will_paginate_installed?
         @mock_model_class.should_receive(:paginate).with(an_instance_of(Hash)).and_return([mock_model_instance])
       else
         @mock_model_class.should_receive(:find).with(:all, an_instance_of(Hash)).and_return([mock_model_instance])
-      end
+      end      
       get :index
       assigns[:records].should == [mock_model_instance]
     end
@@ -49,7 +64,7 @@ describe 'basic administrate_me', :shared => true do
 
       it "should render all models as xml" do
         request.env["HTTP_ACCEPT"] = "application/xml"
-        if @will_paginate_installed
+        if will_paginate_installed?
           @mock_model_class.should_receive(:paginate).with(an_instance_of(Hash)).and_return(models = mock("Array of models"))
         else
           @mock_model_class.should_receive(:find).with(:all, an_instance_of(Hash)).and_return(models = mock("Array of models"))
@@ -116,7 +131,7 @@ describe 'basic administrate_me', :shared => true do
         assigns(:resource).should equal(mock_model_instance)
       end
 
-      it "should redirect to the created model" do
+      it "should redirect to the models list" do
         @mock_model_class.stub!(:new).and_return(mock_model_instance(:save! => true))
         controller.stub!(:path_to_index).and_return('/index/path')
         post :create, :resource_name => {}
@@ -128,7 +143,7 @@ describe 'basic administrate_me', :shared => true do
     describe "with invalid params" do
 
       it "should expose a newly created but unsaved model as @resource" do
-        @mock_model_class.stub!(:new).with({'these' => 'params'}).and_return(mock_model_instance(:save! => false))
+        @mock_model_class.should_receive(:new).with({'these' => 'params'}).and_return(mock_model_instance(:save! => false))
         controller.stub!(:model_name).and_return(:resource_name)
         post :create, :resource_name => {:these => 'params'}
         assigns(:resource).should equal(mock_model_instance)
@@ -203,8 +218,9 @@ describe 'basic administrate_me', :shared => true do
   end
 
   describe "responding to DELETE destroy" do
-
-    it "should destroy the requested model" do
+    
+    
+    it "should destroy the requested model" do      
       @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
       mock_model_instance.should_receive(:destroy)
       delete :destroy, :id => "37"
@@ -215,6 +231,204 @@ describe 'basic administrate_me', :shared => true do
       controller.stub!(:path_to_index).and_return('/index/path')
       delete :destroy, :id => "1"
       response.should redirect_to('/index/path')
+    end
+
+  end
+
+end
+
+describe 'basic administrate_me with parent', :shared => true do
+
+  before do
+    parent_class.should_receive(:find).with(mock_model_parent.id).and_return(mock_model_parent)
+  end
+
+  def mock_model_instance(stubs={})
+    @mock_model_instance ||= mock_model(@mock_model_class, stubs)    
+  end
+
+  def parent_parameter_id
+    "#{@parent_name}_id"
+  end
+
+  def parent_class
+    @parent_class ||= @parent_name.classify.constantize
+  end
+
+  def mock_model_parent
+    @mock_model_parent ||= mock_model(parent_class, :id => '42')
+  end
+
+  def will_paginate_installed?
+    @mock_model_class.respond_to?('paginate')
+  end
+
+  describe "responding to GET index" do
+  
+    it "should expose all models as @records" do
+      if will_paginate_installed?
+        @mock_model_class.should_receive(:paginate).with(an_instance_of(Hash)).and_return([mock_model_instance])
+      else
+        @mock_model_class.should_receive(:find).with(:all, an_instance_of(Hash)).and_return([mock_model_instance])
+      end      
+      get :index, parent_parameter_id => mock_model_parent.id
+      assigns[:records].should == [mock_model_instance]
+    end
+
+    describe "with mime type of xml" do
+
+      it "should render all models as xml" do
+        request.env["HTTP_ACCEPT"] = "application/xml"
+        if will_paginate_installed?
+          @mock_model_class.should_receive(:paginate).with(an_instance_of(Hash)).and_return(models = mock("Array of models"))
+        else
+          @mock_model_class.should_receive(:find).with(:all, an_instance_of(Hash)).and_return(models = mock("Array of models"))
+        end
+        models.should_receive(:to_xml).and_return("generated XML")
+        get :index, parent_parameter_id => mock_model_parent.id
+        response.body.should == "generated XML"
+      end
+
+    end
+
+  end 
+  
+  describe "responding to GET show" do
+
+    it "should expose the requested model as @resource" do
+      @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
+      get :show, parent_parameter_id => mock_model_parent.id, :id => "37"
+      assigns[:resource].should equal(mock_model_instance)
+    end
+
+  describe "with mime type of xml" do
+
+      it "should render the requested model as xml" do
+        request.env["HTTP_ACCEPT"] = "application/xml"
+        @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
+        mock_model_instance.should_receive(:to_xml).and_return("generated XML")
+        get :show, parent_parameter_id => mock_model_parent.id, :id => "37"
+        response.body.should == "generated XML"
+      end
+
+    end
+
+  end  
+  
+ describe "responding to GET new" do
+
+    it "should expose a new model as @resource" do
+      @mock_model_class.should_receive(:new).and_return(mock_model_instance)
+      get :new, parent_parameter_id => mock_model_parent.id
+      assigns[:resource].should equal(mock_model_instance)
+    end
+
+  end
+
+  describe "responding to GET edit" do
+
+    it "should expose the requested model as @resource" do
+      @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
+      get :edit, parent_parameter_id => mock_model_parent.id, :id => "37"
+      assigns[:resource].should equal(mock_model_instance)
+    end
+
+  end
+  
+  describe "responding to POST create" do
+
+    describe "with valid params" do
+
+      it "should expose a newly created model as @resource" do
+        @mock_model_class.should_receive(:new).with({'these' => 'params', parent_parameter_id => mock_model_parent.id}).and_return(mock_model_instance(:save! => true))
+        controller.stub!(:model_name).and_return(:resource_name)
+        post :create, :resource_name => {:these => 'params'}, parent_parameter_id => mock_model_parent.id
+        assigns(:resource).should equal(mock_model_instance)
+      end
+
+      it "should redirect to the models list" do
+        @mock_model_class.stub!(:new).and_return(mock_model_instance(:save! => true))
+        controller.stub!(:model_name).and_return(:resource_name)
+        controller.stub!(:path_to_index).and_return('/index/path')
+        post :create, :resource_name => {}, parent_parameter_id => mock_model_parent.id
+        response.should redirect_to('/index/path')
+      end
+
+    end
+
+    describe "with invalid params" do
+
+      it "should expose a newly created but unsaved model as @resource" do
+        @mock_model_class.should_receive(:new).with('these' => 'params', parent_parameter_id => mock_model_parent.id).and_return(mock_model_instance(:save! => false))
+        controller.stub!(:model_name).and_return(:resource_name)
+        post :create, :resource_name => {:these => 'params'}, parent_parameter_id => mock_model_parent.id
+        assigns(:resource).should equal(mock_model_instance)
+      end
+
+      it "should re-render the 'base_form' template" do
+        @mock_model_class.stub!(:new).and_return(mock_model_instance(:save! => false))
+        controller.stub!(:model_name).and_return(:resource_name)
+        post :create, :resource_name => {}, parent_parameter_id => mock_model_parent.id
+        response.should render_template('commons/base_form')
+      end
+
+    end
+
+  end
+
+  describe "responding to PUT udpate" do
+
+    describe "with valid params" do
+
+      it "should update the requested model" do
+        @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
+        mock_model_instance.should_receive(:attributes=).with({'these' => 'params'})
+        mock_model_instance.should_receive(:save!).and_return(true)
+        controller.stub!(:model_name).and_return(:resource_name)
+        put :update, :id => "37", :resource_name => {:these => 'params'}, parent_parameter_id => mock_model_parent.id
+      end
+
+      it "should expose the requested model as @resource" do
+        @mock_model_class.stub!(:find).and_return(mock_model_instance(:attributes= => true))
+        mock_model_instance.should_receive(:save!).and_return(true)
+        put :update, :id => "1", parent_parameter_id => mock_model_parent.id
+        assigns(:resource).should equal(mock_model_instance)
+      end
+
+      it "should redirect to the model" do
+        @mock_model_class.stub!(:find).and_return(mock_model_instance(:attributes= => true))
+        mock_model_instance.should_receive(:save!).and_return(true)
+        controller.stub!(:path_to_element).and_return('/resource/path')
+        put :update, :id => "1", parent_parameter_id => mock_model_parent.id
+        response.should redirect_to('/resource/path')
+      end
+
+    end
+
+    describe "with invalid params" do
+
+      it "should update the requested model" do
+        @mock_model_class.should_receive(:find).with("37").and_return(mock_model_instance)
+        mock_model_instance.should_receive(:attributes=).with({'these' => 'params'})
+        mock_model_instance.should_receive(:save!).and_return(false)
+        controller.stub!(:model_name).and_return(:resource_name)
+        put :update, :id => "37", :resource_name => {:these => 'params'}, parent_parameter_id => mock_model_parent.id
+      end
+
+      it "should expose the model as @resource" do
+        @mock_model_class.stub!(:find).and_return(mock_model_instance(:attributes= => false))
+        mock_model_instance.should_receive(:save!).and_return(false)
+        put :update, :id => "1", parent_parameter_id => mock_model_parent.id
+        assigns(:resource).should equal(mock_model_instance)
+      end
+
+      it "should re-render the 'base_form' template" do
+        @mock_model_class.stub!(:find).and_return(mock_model_instance(:attributes= => false))
+        mock_model_instance.should_receive(:save!).and_return(false)
+        put :update, :id => "1", parent_parameter_id => mock_model_parent.id
+        response.should render_template('commons/base_form')
+      end
+
     end
 
   end
